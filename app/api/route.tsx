57146fb3afec2b3728109abe5894f81aa3d110fa
Jafar Aziz <aziz@jafaraziz.com>
2023-05-16 15:08:34 +0700

    example-routes

---
+++ app/api/route.tsx
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const id = searchParams.get('id');
+ 
+  return NextResponse.json({ length: id?.length });
+}
